c9a175770b12efef016e26c0808931119a25d367
cmayer <Christoph.Mayer@cern.ch>
2018-06-27 18:04:15 +0200

    check for errors per file and windows compatibility for kiwi_info.m

--- kiwi_info.m
+++ kiwi_info.m
@@ -1,7 +1,13 @@
 ## -*- octave -*-
 
 function d=kiwi_info()
-  [_,a]=system('cat gnss_pos/*.txt');
-  eval(a);
+  files = glob('gnss_pos/*.txt');
+  for i=1:length(files)
+    try
+      source(files{i});
+    catch err
+      error(sprintf('%s: %s', files{i}, err.message))
+    end_try_catch
+  end
 end
 
